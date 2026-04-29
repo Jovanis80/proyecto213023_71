581a8305eb4d5bfa17133a2b59a340bbad72c4c4
Jovanis80 <javanegasro@unadvirtual.edu.co>
2026-04-28 20:17:56 -0500

    Update excepciones.py

--- excepciones.py
+++ excepciones.py
@@ -1,1 +1,11 @@
+class ClienteError(Exception):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
 
+class ServicioError(Exception):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
+class ReservaError(Exception):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
