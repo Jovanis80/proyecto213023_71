93e59e28d0ce0c96202d9c4714bdde793dbff291
Jovanis80 <javanegasro@unadvirtual.edu.co>
2026-04-29 14:29:25 -0500

    Delete logger.py

--- logger.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def registrar_log(mensaje):
-    with open("logs.txt", "a", encoding="utf-8") as f:
-        fecha = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        f.write(f"[{fecha}] {mensaje}\n")
