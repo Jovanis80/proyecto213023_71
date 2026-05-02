74c2ccf0fa5c05d03853666b391e17372d197c11
jesuscharris729-del <jesuscharris729@gmail.com>
2026-05-02 16:28:25 -0500

    Fix constructor and improve error messages in Servicio

--- servicio.py
+++ servicio.py
@@ -1,21 +1,23 @@
 from abc import ABC, abstractmethod
 from excepciones import ServicioError
 
+
 class Servicio(ABC):
 
-    def __init__(self, nonbre, tarifa_base):
-        if not nonbre:
-            raise ServicioError("el nombre del servicio no puede estar vacio")
-        if tarifa_base  <= 0:
-            raise ServicioError("La tarfa debe ser mayar que 0")
-        
-    self.nombre = nombre
-    self.tarifa_base = tarifa_base
+    def __init__(self, nombre, tarifa_base):
+        if not nombre:
+            raise ServicioError("El nombre del servicio no puede estar vacío")
 
-@abstractmethod
-def calcular_costo(self, duracion, descuento=0)
-    pass
+        if tarifa_base <= 0:
+            raise ServicioError("La tarifa debe ser mayor que 0")
 
-@abstractmethod
-def descripcion(self):
-    pass
+        self.nombre = nombre
+        self.tarifa_base = tarifa_base
+
+    @abstractmethod
+    def calcular_costo(self, duracion, descuento=0):
+        pass
+
+    @abstractmethod
+    def descripcion(self):
+        pass
